f3647a6c55c6b5bed1425525f818f2e129fe36ce
DavidWilde <davidgluttongk@outlook.com>
2020-08-29 11:07:42 -0700

    Advanced
    Add Bomb that kills all the zombies
    Add food bonus for clearing all shrubbery

--- Assets/Scripts/ScoreManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ScoreManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    private bool reward = true;
-}
