546cdc93ca9d22c7fc0304831d0c118ebe61a47f
davidwilde <davidgluttongk@outlook.com>
2021-11-27 18:13:52 +0800

    test

---
+++ Assets/_Complete-Game/Scripts/RewardManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Data;
+using UnityEngine;
+
+public class RewardManager : MonoBehaviour
+{
+    public RewardSettings RewardSettings => rewardSettings;
+    [SerializeField] private RewardSettings rewardSettings;
+    
+    [SerializeField]
+    private int valueOfAwardByEnemy = 10; //每个敌人获取的奖励值
+    [SerializeField]
+    private int valueOfAwardByTime = 5;//基于时间获得的奖励
+    [SerializeField]
+    private int maxTimeToEarnReward = 30;
+    public int MaxTimeToEarnReward => maxTimeToEarnReward;
+
+    public string rewardMessage = null;
+    
+    public bool IsRewarding { get; private set; } = true;
+
+    public void SetIsReward(bool value)
+    {
+        IsRewarding = value;
+    }
+
+    // TODO: instance
+    // public static RewardManager instance { get; private set; }
+    //
+    // private void Awake()
+    // {
+    //     // throw new NotImplementedException();
+    //     
+    // }
+}
---
+++ Assets/_Complete-Game/Scripts/RewardSettings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "David Wilde/Reward Settings")]
+    public class RewardSettings : ScriptableObject
+    {
+        public int ValueOfAwardByEnemy => valueOfAwardByEnemy;
+        public int MaxShrubberyReward => maxShrubberyReward;
+        
+        [SerializeField]
+        [Range(0,  50)]
+        private int valueOfAwardByEnemy = 10;
+
+        [SerializeField] [Range(1, 15)] private int valueOfAwardByTime = 5;
+
+        [SerializeField] [Range(15, 60)] private int maxTimeToEarnReward = 30;
+        
+        [SerializeField] [Range(5, 15)] private int maxShrubberyReward = 5;
+    }
+}
